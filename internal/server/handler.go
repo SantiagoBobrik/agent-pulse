84d8179cf33007b297798b7af7fd54ad38762d0a
Santiago Bobrik <santiago.bobrik@gmail.com>
2026-03-01 17:35:42 -0300

    feat: include provider information in event logging

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -25,7 +25,7 @@ func handleEvent(dispatcher *Dispatcher) http.HandlerFunc {
 		if len(eventPayload.Data) > 0 {
 			json.Unmarshal(eventPayload.Data, &data)
 		}
-		slog.Info("event received", "type", eventPayload.Type, "data", data)
+		slog.Info("event received", "type", eventPayload.Type, "provider", eventPayload.Provider, "data", data)
 		dispatcher.Dispatch(eventPayload)
 		w.WriteHeader(http.StatusAccepted)
 	}
